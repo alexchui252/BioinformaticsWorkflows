1e61820ecff2c774baaaa5d372b80b666534b75e
alexc <alexchui252@gmail.com>
2021-05-14 10:02:11 -0400

    replaced JS for setup, describe

--- js/common-page-elements.js
+++ js/common-page-elements.js
@@ -1,6 +1,34 @@
+//head
+const head = document.querySelector('head');
+
+head.innerHTML = 
+`
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${page_name} - BioinformaticsWorkflows</title>
+<link rel="stylesheet" href="../styles/styles.css">
+`;
+
+//header
+const header = document.querySelector('header');
+
+header.innerHTML = 
+`
+<div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="../index.html">Home</a>
+      <a href="../directory.html">Directory</a>
+      <a href="../updates.html">Latest Updates</a>
+      <a href="../about.html">About</a>
+      <a href="../contact.html">Contact</a>
+</div>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+<h1>BioinformaticsWorkflows</h1>
+`;
+
+//non-home page banner
 const banner = document.querySelector('.banner');
 
-//console.log(banner.innerHTML);
 banner.innerHTML = 
 `
 <img src="../img/banner-background-cut.jpg" alt="banner image">
@@ -9,6 +37,7 @@ banner.innerHTML =
 </div>
 `;
 
+//side menu functions
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
@@ -16,3 +45,13 @@ function openNav() {
 function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
 }
+
+//footer
+const body = document.querySelector('body');
+
+body.innerHTML += 
+`
+<footer>
+      <p class="copyright">Copyright 2021 BioinformaticsWorkflows</p>
+    </footer>
+`;
